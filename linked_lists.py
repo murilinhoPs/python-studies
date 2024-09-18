358d3f3b238a94ad9f2cb26a32a047e16d6d8f62
murilinhops <murilo.pescatori@nubank.com.br>
2024-09-18 10:59:12 -0300

    add check to add at the end if index is the size of the list

--- linked_lists.py
+++ linked_lists.py
@@ -11,6 +11,17 @@ class LinkedList:
     def __init__(self, head_node: Node):
         self.head = head_node
 
+    def size(self):
+        size = 0
+        if self.head:
+            current_node = self.head
+            while current_node:
+                size += 1
+                current_node = current_node.next
+            return size
+        else:
+            return 0
+
     def insert_at_beginning(self, data: str):
         new_node = Node(data)
         if self.head is None:
@@ -34,13 +45,20 @@ class LinkedList:
 
     def insert_at_index(self, data: str, target_index=0):
         if target_index == 0:
+            print("insert_at_beginning")
             self.insert_at_beginning(data)
             return
+
+        if target_index == self.size():
+            print("insert_at_end")
+            self.insert_at_end(data)
+            return
+        
         # passar pelos elementos da lista até encontrar o index desejado
         # ir somando no current_index++ até que ele seja engual o index atual, atualizar o current_node com o next dele toda vez que somar +1
         # achei o index certo!
         # criar o new_node, o next dele vai ser o current_node.next e o current_node.next = new_node
-        current_index = 1  # starts at 1
+        current_index = 1  # starts at 1, if is 0 insert_at_beginning (up)
         current_node = self.head  # primeiro index (node)
         while current_node != None and current_index != target_index:
             current_index += 1
@@ -62,17 +80,6 @@ class LinkedList:
             if current_node.next != None:
                 print(f"next -> {current_node.next.data}")
             current_node = current_node.next
-    
-    def size(self):
-        size = 0
-        if(self.head):
-            current_node = self.head
-            while(current_node):
-                size = size+1
-                current_node = current_node.next
-            return size
-        else:
-            return 0
 
 
 initial_node = Node("murilo")
@@ -80,6 +87,6 @@ new_ll = LinkedList(initial_node)
 new_ll.insert_at_end("murilinho")
 new_ll.insert_at_beginning("muri")
 print(new_ll.size())
-new_ll.insert_at_index("djdjd", target_index=2)
+new_ll.insert_at_index("djdjd", target_index=3)
 
 new_ll.print_LL()
