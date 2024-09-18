50b571d788a67918deeac5b309354d12a6e3adbb
murilinhops <murilo.pescatori@nubank.com.br>
2024-09-18 10:52:30 -0300

    Add insert_at_index on a linked_list

--- linked_lists.py
+++ linked_lists.py
@@ -13,43 +13,73 @@ class LinkedList:
 
     def insert_at_beginning(self, data: str):
         new_node = Node(data)
-
-        if self.head is None:
-            self.head = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
-
-    def insert_at_end(self, data: str):
-        new_node = Node(data)
-
         if self.head is None:
             self.head = new_node
             return
 
-        current_node = self.head
-        while (
-            current_node.next
-        ):  # passa por todos elementos da lista, quando um next dele for Null adiciona o novo node como next do current_node
-            current_node = current_node.next
+        new_node.next = self.head
+        self.head = new_node
 
+    def insert_at_end(self, data: str):
+        if self.head is None:
+            self.insert_at_beginning(data)
+            return
+
+        new_node = Node(data)
+        current_node = self.head
+        # passa por todos elementos da lista, quando um next dele for Null adiciona o novo node como next do current_node
+        while current_node.next:
+            current_node = current_node.next
         current_node.next = new_node
 
-    def insert_at_index(self, data: str, index=0):
-        return
+    def insert_at_index(self, data: str, target_index=0):
+        if target_index == 0:
+            self.insert_at_beginning(data)
+            return
+        # passar pelos elementos da lista até encontrar o index desejado
+        # ir somando no current_index++ até que ele seja engual o index atual, atualizar o current_node com o next dele toda vez que somar +1
+        # achei o index certo!
+        # criar o new_node, o next dele vai ser o current_node.next e o current_node.next = new_node
+        current_index = 1  # starts at 1
+        current_node = self.head  # primeiro index (node)
+        while current_node != None and current_index != target_index:
+            current_index += 1
+            current_node = current_node.next
+
+        if current_node is None:
+            print("index não existe")
+            raise Exception("Index is out of bounds")
+            return
+
+        new_node = Node(data)
+        new_node.next = current_node.next
+        current_node.next = new_node
 
     def print_LL(self):
         current_node = self.head
         while current_node:
             print(current_node.data)
             if current_node.next != None:
-                print(f'next -> {current_node.next.data}')
+                print(f"next -> {current_node.next.data}")
             current_node = current_node.next
+    
+    def size(self):
+        size = 0
+        if(self.head):
+            current_node = self.head
+            while(current_node):
+                size = size+1
+                current_node = current_node.next
+            return size
+        else:
+            return 0
 
 
 initial_node = Node("murilo")
 new_ll = LinkedList(initial_node)
 new_ll.insert_at_end("murilinho")
 new_ll.insert_at_beginning("muri")
+print(new_ll.size())
+new_ll.insert_at_index("djdjd", target_index=2)
 
 new_ll.print_LL()
