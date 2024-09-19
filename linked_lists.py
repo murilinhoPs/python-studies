ebf7d6915c392632c71ce9ba492700b8920276a3
murilinhops <murilinhorps@gmail.com>
2024-09-18 21:49:08 -0300

    .

--- linked_lists.py
+++ linked_lists.py
@@ -38,7 +38,8 @@ class LinkedList:
 
         new_node = Node(data)
         current_node = self.head
-        # passa por todos elementos da lista, quando um next dele for Null adiciona o novo node como next do current_node
+        # passa por todos elementos da lista, quando um next dele for Null adiciona o novo node como next do
+        # current_node
         while current_node.next:
             current_node = current_node.next
         current_node.next = new_node
@@ -53,67 +54,67 @@ class LinkedList:
             print("insert_at_end")
             self.insert_at_end(data)
             return
-        
+
         # passar pelos elementos da lista até encontrar o index desejado
-        # ir somando no current_index++ até que ele seja engual o index atual, atualizar o current_node com o next dele toda vez que somar +1
+        # ir somando no current_index++ até que ele seja engual o index atual;
+        # atualizar o current_node com o next dele toda vez que somar +1
         # achei o index certo!
         # criar o new_node, o next dele vai ser o current_node.next e o current_node.next = new_node
+
         current_index = 1  # starts at 1, if is 0 insert_at_beginning (up)
-        current_node = self.head  # primeiro index (node)
-        while current_node != None and current_index != target_index:
+        current_node = self.head  # first index (node)
+        while current_node is not None and current_index != target_index:
             current_index += 1
             current_node = current_node.next
 
         if current_node is None:
             print("index não existe")
             raise Exception("Index is out of bounds")
-            return
 
         new_node = Node(data)
         new_node.next = current_node.next
         current_node.next = new_node
-        
+
     def update_index(self, new_value: str, target_index=0):
         if target_index == 0:
             self.head.data = new_value
             return
-        
+
         current_index = 1
         current_node = self.head
-        while current_node != None and current_index != target_index:
+        while current_node is not None and current_index != target_index:
             current_index += 1
             current_node = current_node.next
 
         if current_node is None:
             print("index não existe")
             raise Exception("Index is out of bounds")
-            return
 
         current_node.data = new_value
-        
+
     def remove_first(self):
         if self.head is None:
             return
 
         self.head = self.head.next
-        
+
     def remove_last(self):
         current_node = self.head
         if current_node is None:
             return
 
-        while current_node.next.next != None:
+        while current_node.next.next is not None:
             print(f'current: {current_node.data}')
             current_node = current_node.next
 
-        print(f'second last: {current_node.data}') 
+        print(f'second last: {current_node.data}')
         current_node.next = None  # vai remover o proximo dele, que é realmente oo último elemento
 
-    def print_LL(self):
+    def print_ll(self):
         current_node = self.head
         while current_node:
             print(current_node.data)
-            if current_node.next != None:
+            if current_node.next is not None:
                 print(f"next -> {current_node.next.data}")
             current_node = current_node.next
 
@@ -124,8 +125,8 @@ new_ll.insert_at_end("murilinho")
 new_ll.insert_at_beginning("muri")
 new_ll.insert_at_index("djdjd", target_index=1)
 print(new_ll.size())
-new_ll.print_LL()
+new_ll.print_ll()
 new_ll.update_index("jorge", 2)  # atualizar o 2 elemento
 print("")
 new_ll.remove_last()
-new_ll.print_LL()
+new_ll.print_ll()
